06280cacef98d85d356f5980dac29ef39f3a48c4
wubbadukky <goosecollins13@hotmail.com>
2020-09-19 01:06:06 -0700

    Division function added

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
 def add(num1, num2):
     return int(num1) + int(num2)
 
+def divide(num1, num2):
+    return  int(num1) / int(num2)
+
 if __name__ == "__main__":
     n1 = input("Enter the first number: ")
     n2 = input("Enter the second number: ")
