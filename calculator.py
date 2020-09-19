10c208c9fc3457b4baa6b3d17e8c8e8983219e12
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2020-09-18 23:39:12 -0700

    Multiplication feature added

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
 def add(num1, num2):
     return int(num1) + int(num2)
 
+def multiply(num1, num2):
+    return sum([num1 for i in range(num2)])
+
 if __name__ == "__main__":
     n1 = input("Enter the first number: ")
     n2 = input("Enter the second number: ")
