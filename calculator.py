0c1217de91135239acf8daa769177c6a078e9abf
wubbadukky <goosecollins13@hotmail.com>
2020-09-19 14:20:25 -0700

    made answer value

--- calculator.py
+++ calculator.py
@@ -2,8 +2,12 @@ def add(num1, num2):
     return int(num1) + int(num2)
 
 def divide(num1, num2):
-    return  int(num1) / int(num2)
+    answer = int(num1) / int(num2)
+    return  int(answer)
 
 if __name__ == "__main__":
     n1 = input("Enter the first number: ")
     n2 = input("Enter the second number: ")
+
+
+print(divide(n1,n2))
