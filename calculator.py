61f2e642b6a6ef4ac4ec8dbec685ce39aa255062
wubbadukky <goosecollins13@hotmail.com>
2020-09-19 15:28:11 -0700

    Got rid of an unnecessary semicolon

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 def exponent(num1, num2):
-    return int(num1)**int(num2);  #returns num1^num2
+    return int(num1)**int(num2)  #returns num1^num2
 
 if __name__ == "__main__":
     n1 = input("Enter the first number: ")
     n2 = input("Enter the second number: ")
+
