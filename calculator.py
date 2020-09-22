79a8e8ca13396c38afb801a46e6cee9c4fa3ec3b
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2020-09-21 18:12:24 -0700

    menu feature finished

--- calculator.py
+++ calculator.py
@@ -9,5 +9,15 @@ def divide(num1, num2):
     return  int(answer)
 
 if __name__ == "__main__":
-    n1 = input("Enter the first number: ")
-    n2 = input("Enter the second number: ")
+    n1 = int(input("Enter the first number: "))
+    n2 = int(input("Enter the second number: "))
+    mode = input("Enter 1 to multiply / 2 to divide / 3 to use num1 exponent num2 / else to exit: ")
+
+    if mode == "1":
+        print(str(n1) + " * " + str(n2) + " is " + str(multiply(n1,n2)) + ".")
+    elif mode == "2":
+        print(str(n1) + " / " + str(n2) + " is " + str(divide(n1,n2)) + ".")
+    elif mode == "3":
+        print(str(n1) + " ^ " + str(n2) + " is " + str(exponent(n1,n2)) + ".")
+    else:
+        print("Good bye!")
